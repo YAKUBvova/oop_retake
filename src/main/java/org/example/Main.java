92d57fc4f02ebe75779eeed9312992ffcb511375
YAKUB <yakub.voxa4@gmail.com>
2023-10-19 12:48:16 +0300

    Many numbers

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args){
         String_Calc str = new String_Calc();
-        int result = str.add("1,22 numbers2");
+        int result = str.add("1,22,3,5,6,5");
         System.out.println(result);
     }
 
--- src/main/java/org/example/String_Calc.java
+++ src/main/java/org/example/String_Calc.java
@@ -1,14 +1,47 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class String_Calc {
+    public int adding(String numbers){
+        int result = 0 ;
+        int start = 0 ;
+        int end_ ;
+        int i = 0;
+        while (i<numbers.length()){
+
+                try{
+                    if (numbers.startsWith(",", i)) {
+                        end_ = i;
+                        i += 1;
+                        try {
+                            int number = Integer.parseInt(numbers.substring(start, end_));
+                            result += number;
+                        } catch (NumberFormatException e) {
+                            throw new NumberFormatException("Error! Invalid format");
+                        }
+                        start = end_ + 1;
+                        if (start == numbers.length()) {
+                            throw new NumberFormatException("Error! Invalid format");
+                        }
+
+                    }
+                }catch (StringIndexOutOfBoundsException e){
+                    System.out.print("indexOut");
+                }
+
+            if (i == numbers.length()-1) {
+                result += Integer.parseInt(numbers.substring(start));
+                return result ;
+            }
+            i++;
+        }
+        return Integer.parseInt(numbers) ;
+    }
 
     public int add (String numbers){
         if (numbers.length() == 0) return 0 ;
-        if (numbers.contains(",")) {
-            return Integer.parseInt(numbers.substring(0,numbers.indexOf(",")))+
-                   Integer.parseInt(numbers.substring(numbers.indexOf(",")+1));
-        }
-        else return Integer.parseInt(numbers);
+        return adding(numbers);
     }
 }
