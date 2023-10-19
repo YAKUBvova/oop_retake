c58c483f30983050e515c17bc0208be4042232ad
YAKUB <yakub.voxa4@gmail.com>
2023-10-19 12:06:00 +0300

    1 step

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args){
+        String_Calc str = new String_Calc();
+        int result = str.add("");
+        System.out.println(result);
+    }
+
+}
